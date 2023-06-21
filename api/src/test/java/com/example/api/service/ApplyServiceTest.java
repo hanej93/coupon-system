2e52f50272a44255064d9c2f8ce0c76c8272337b
EJ <hanej93@naver.com>
2023-06-21 11:52:56 +0900

    문제점(Race Condition 이슈)

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -3,6 +3,10 @@ package com.example.api.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +31,30 @@ class ApplyServiceTest {
 		// then
 		long count = couponRepository.count();
 		assertThat(count).isEqualTo(1L);
+	}
+
+	@Test
+	void 여러명응모() throws Exception {
+		int threadCount = 1000;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			long userId = i;
+			executorService.submit(() -> {
+				try {
+					applyService.apply(userId);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+
+		long count = couponRepository.count();
+
+		assertThat(count).isEqualTo(100L);
 	}
 
 }
