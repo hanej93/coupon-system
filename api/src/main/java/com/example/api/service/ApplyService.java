d22702fc147d35a83d34b40e204c202927ee0415
EJ <hanej93@naver.com>
2023-06-21 14:22:46 +0900

    문제점 해결하기

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -3,19 +3,22 @@ package com.example.api.service;
 import org.springframework.stereotype.Service;
 
 import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 
 @Service
 public class ApplyService {
 
 	private final CouponRepository couponRepository;
+	private final CouponCountRepository couponCountRepository;
 
-	public ApplyService(CouponRepository couponRepository) {
+	public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
 		this.couponRepository = couponRepository;
+		this.couponCountRepository = couponCountRepository;
 	}
 
 	public void apply(Long userId) {
-		long count = couponRepository.count();
+		Long count = couponCountRepository.increment();
 
 		if (count > 100) {
 			return;
