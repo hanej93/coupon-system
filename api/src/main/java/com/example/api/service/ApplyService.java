c2a70fdabdea43ae3d71c63d49f97e1cba97f06e
EJ <hanej93@naver.com>
2023-06-23 05:58:57 +0900

    Producer 사용하기

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -3,6 +3,7 @@ package com.example.api.service;
 import org.springframework.stereotype.Service;
 
 import com.example.api.domain.Coupon;
+import com.example.api.producer.CouponCreateProducer;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 
@@ -11,10 +12,13 @@ public class ApplyService {
 
 	private final CouponRepository couponRepository;
 	private final CouponCountRepository couponCountRepository;
+	private final CouponCreateProducer couponCreateProducer;
 
-	public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
+	public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository,
+		CouponCreateProducer couponCreateProducer) {
 		this.couponRepository = couponRepository;
 		this.couponCountRepository = couponCountRepository;
+		this.couponCreateProducer = couponCreateProducer;
 	}
 
 	public void apply(Long userId) {
@@ -24,6 +28,6 @@ public class ApplyService {
 			return;
 		}
 
-		couponRepository.save(new Coupon(userId));
+		couponCreateProducer.create(userId);
 	}
 }
